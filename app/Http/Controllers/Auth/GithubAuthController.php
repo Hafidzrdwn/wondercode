393efa376e65ab6163e131232b8998b93dfb7979
Hafidzrdwn <hafidz10032005@gmail.com>
2023-01-07 02:02:09 +0700

    memperbaiki kodingan auth socialite github

--- app/Http/Controllers/Auth/GithubAuthController.php
+++ app/Http/Controllers/Auth/GithubAuthController.php
@@ -3,56 +3,25 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use App\Models\User;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use Laravel\Socialite\Facades\Socialite;
-use RealRashid\SweetAlert\Facades\Alert;
 
 class GithubAuthController extends Controller
 {
-    public function redirect(){
-
+    public function redirect()
+    {
         return Socialite::driver('github')->redirect();
-
     }
 
-    public function callback(){
+    public function callback()
+    {
 
         try {
-
-            $github_user = Socialite::driver('github')->user();
-            $checkEmail = User::where('email', $github_user->getEmail())->first();
-
-            if($checkEmail) {
-                User::find($checkEmail->id)->update(
-                    [
-                        'social_id'=> $github_user->getId(),
-                        'auth_type'=>'github'
-                    ]);
-                Auth::login($checkEmail);
-
-                Alert::toast('Anda Telah Login', 'success');
-                return redirect()->route('home');
-            } else {
-                $new_user = User::create([
-                    'username' => $github_user->getName(),
-                    'email' => $github_user->getEmail(),
-                    'password' => null,
-                    'social_id' => $github_user->getId(),
-                    'auth_type' => 'github'
-                ]);
-
-                Auth::login($new_user);
-                
-                Alert::toast('Anda Telah Login', 'success');
-                return redirect()->route('home');
-            }
-
-
+            $user = Socialite::driver('github')->user();
         } catch (\Throwable $th) {
-            dd('Something wrong' .$th->getMessage());
+            dd('Something wrong' . $th->getMessage());
         }
 
+        $social_service = new SocialAuthServiceController();
+        return $social_service->findOrCreateAccount($user, 'github');
     }
 }
