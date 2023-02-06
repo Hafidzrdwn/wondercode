bb582f614cb886011b2e722593bb214a8a0b5a21
Dhamarasp <dhamarasp.10rpl1@gmail.com>
2023-02-06 11:07:04 +0700

    [Dhamar] Menambahkan landing page baru untuk halaman login dan master data

---
+++ resources/views/admin/auth/login.blade.php
@@ -0,0 +1,53 @@
+@extends('layouts.admin.auth')
+@section('title', "Admin")
+
+@section('content')
+<div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
+    <div class="login-brand">
+        <img src="{{ asset('assets/images/logo-web.png') }}" alt="logo" width="100" class="shadow-light">
+    </div>
+
+    <div class="card card-primary">
+        <div class="card-header">
+            <h4>Login</h4>
+        </div>
+
+        <div class="card-body">
+            <form method="POST" action="{{ route('auth.admin.login') }}" class="needs-validation" novalidate="">
+                @csrf
+                <div class="form-group">
+                    <label for="email">Email</label>
+                    <input id="email" type="email" class="form-control" name="email" tabindex="1" required autofocus>
+                    <div class="invalid-feedback">
+                        Please fill in your email
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <div class="d-block">
+                        <label for="password" class="control-label">Password</label>
+                        <div class="float-right">
+                            <a href="auth-forgot-password.html" class="text-small">
+                                Forgot Password?
+                            </a>
+                        </div>
+                    </div>
+                    <input id="password" type="password" class="form-control" name="password" tabindex="2" required>
+                    <div class="invalid-feedback">
+                        please fill in your password
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <button type="submit" class="btn btn-primary btn-lg btn-block" tabindex="4">
+                        Login
+                    </button>
+                </div>
+            </form>
+        </div>
+    </div>
+    <div class="simple-footer">
+        Copyright &copy; WonderCode {{ date('Y') }}
+    </div>
+</div>
+@endsection
