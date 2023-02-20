806c5262c7e6611c07c895c16b1a6a892e117e3a
Dhamarasp <dhamarasp.10rpl1@gmail.com>
2023-02-20 11:56:50 +0700

    [Dhamar] Menambahkan navbar active jika url berada di view edit

--- resources/views/layouts/admin/partials/sidebar.blade.php
+++ resources/views/layouts/admin/partials/sidebar.blade.php
@@ -9,10 +9,10 @@
         <li class="@if(Request::is('admin/dashboard')) active @endif"><a class="nav-link"
                 href="{{ route('dashboard.index') }}"><i class="fas fa-fire"></i> <span>Dashboard</span></a></li>
         <li class="menu-header">Master User</li>
-        <li class="nav-item dropdown @if(Request::is('admin/user', 'admin/user-profile', 'admin/user-medsos')) active @endif">
+        <li class="nav-item dropdown @if(Request::is('admin/user*', 'admin/user-profile', 'admin/user-medsos')) active @endif">
             <a href="#" class="nav-link has-dropdown"><i class="fas fa-user"></i><span>Master User</span></a>
             <ul class="dropdown-menu">
-                <li class="@if(Request::is('admin/user')) active @endif"><a class="nav-link" href="{{ route('user.index') }}">Data User</a></li>
+                <li class="@if(Request::is('admin/user*')) active @endif"><a class="nav-link" href="{{ route('user.index') }}">Data User</a></li>
                 <li class="@if(Request::is('admin/user-profile')) active @endif"><a class="nav-link" href="{{ route('user-profile.index') }}">Data User Profile</a></li>
                 <li class="@if(Request::is('admin/user-medsos')) active @endif"><a class="nav-link" href="{{ route('user-medsos.index') }}">Data User Medsos</a></li>
             </ul>
